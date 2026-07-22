4a3e10b5d8be2136c527fd471c872797a3efc7e0
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-07-22 17:40:59 +0200

    Tweaked navbar

--- resources/js/pages/Components/Navbar.vue
+++ resources/js/pages/Components/Navbar.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup lang="ts" xmlns:div="http://www.w3.org/1999/html">
 import {
     NavigationMenu,
     NavigationMenuContent,
@@ -8,29 +8,71 @@ import {
     NavigationMenuList,
     NavigationMenuTrigger,
     NavigationMenuViewport,
+    navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
 import ThemeToggle from '@/pages/Components/ThemeToggle.vue';
+import { LogOut, FileUp } from '@lucide/vue';
+import { Separator } from '@/components/ui/separator';
 </script>
 
 <template>
-    <div class="flex w-full justify-between border border-b px-2 mb-6">
+    <div class="mb-6 flex h-14 w-full justify-between border-b px-2">
         <NavigationMenu class="py-2">
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <NavigationMenuLink>Link</NavigationMenuLink>
-                    </NavigationMenuContent>
+                    <NavigationMenuLink
+                        as-child
+                        :class="[
+                            navigationMenuTriggerStyle(),
+                            'flex-row gap-2',
+                        ]"
+                    >
+                        <a href="#" class="text-emerald-500">
+                            <FileUp class="text-inherit" />
+                            New share
+                        </a>
+                    </NavigationMenuLink>
                 </NavigationMenuItem>
             </NavigationMenuList>
         </NavigationMenu>
         <NavigationMenu class="py-2">
-            <NavigationMenuList>
+            <NavigationMenuList class="h-full items-center space-x-2">
                 <ThemeToggle />
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                    <NavigationMenuTrigger>John Doe</NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <NavigationMenuLink>Link</NavigationMenuLink>
+                        <ul class="grid w-[300px] gap-4">
+                            <li>
+                                <NavigationMenuLink as-child>
+                                    <a href="#">
+                                        <div class="font-medium">Profile</div>
+                                        <div class="text-muted-foreground">
+                                            Tweak your info. 🪪
+                                        </div>
+                                    </a>
+                                </NavigationMenuLink>
+                                <NavigationMenuLink as-child>
+                                    <a href="#">
+                                        <div class="font-medium">My shares</div>
+                                        <div class="text-muted-foreground">
+                                            See your shared files. 🗃️
+                                        </div>
+                                    </a>
+                                </NavigationMenuLink>
+                                <NavigationMenuLink as-child>
+                                    <a href="#">
+                                        <div
+                                            class="font-medium text-destructive"
+                                        >
+                                            Logout
+                                        </div>
+                                        <div class="text-muted-foreground">
+                                            Till we meet again! 👋
+                                        </div>
+                                    </a>
+                                </NavigationMenuLink>
+                            </li>
+                        </ul>
                     </NavigationMenuContent>
                 </NavigationMenuItem>
             </NavigationMenuList>
