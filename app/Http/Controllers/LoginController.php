f67aa8cac98f57de4da24ef78d5056db58c7ddd6
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-07-30 20:03:19 +0200

    Added fallback route when accessing auth protected routes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -22,7 +22,7 @@ class LoginController extends Controller
         if (auth()->attempt($credentials, $remember)) {
             session()->regenerate();
 
-            return to_route('home.index');
+            return redirect()->intended('home.index');
         }
 
         return back()->withErrors(['email' => 'Invalid email or password.']);
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+        $middleware->redirectGuestsTo(fn (Request $request) => route('login.index'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
