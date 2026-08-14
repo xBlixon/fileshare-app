51f4464e6d0a9b8fbf82efa7adafb5442cd8541f
xBlixon <48760619+xBlixon@users.noreply.github.com>
2026-08-14 17:24:04 +0000

    fix code style

--- resources/js/pages/Auth/Login.vue
+++ resources/js/pages/Auth/Login.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { Form } from '@inertiajs/vue3';
-import { login } from '@/actions/App/Http/Controllers/LoginController';
 import { CardFooter } from '@/components/ui/card';
 import FormCheckbox from '@/pages/Components/Form/FormCheckbox.vue';
 import FormError from '@/pages/Components/Form/FormError.vue';
@@ -8,6 +7,7 @@ import FormInput from '@/pages/Components/Form/FormInput.vue';
 import FormLayout from '@/pages/Components/Form/FormLayout.vue';
 import Submit from '@/pages/Components/Form/Submit.vue';
 import Layout from '@/pages/Templates/Layout.vue';
+import { login } from '@/actions/App/Http/Controllers/LoginController';
 
 const route = login();
 </script>
