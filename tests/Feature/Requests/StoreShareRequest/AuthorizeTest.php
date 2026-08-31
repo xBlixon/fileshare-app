8e0594c6a60719702970012b8d89b0d54f7fc511
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-08-31 18:20:20 +0200

    StoreShareRequest tests

---
+++ tests/Feature/Requests/StoreShareRequest/AuthorizeTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Requests\StoreShareRequest;
+
+it('requires the user to be a guest', function () {
+    $request = new StoreShareRequest();
+    $this->assertEquals($request->authorize(), auth()->check());
+});
