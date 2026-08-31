5fbc0849cfd9d3ddf7781578401eef017333e6af
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-08-31 15:53:34 +0200

    Testing User->shares relation

---
+++ tests/Unit/Model/User/UserSharesTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Models\Share;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+it('returns user shares\' relation HasMany', function () {
+    $user = new User();
+    expect($user->shares())->toBeInstanceOf(HasMany::class)
+        ->and($user->shares()->getRelated())->toBeInstanceOf(Share::class);
+});
